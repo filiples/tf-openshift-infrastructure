ced54f541e44ea486bb27d724d54d733918ab3ad
Filip Lešnjaković <filip.lesnjakovic@comsysto.com>
2018-09-14 16:04:04 +0200

    Allow egress ssh traffic

--- networking/security-groups.tf
+++ networking/security-groups.tf
@@ -11,6 +11,13 @@ resource "aws_security_group" "openshift_ssh_sg" {
     cidr_blocks = ["${var.accessip}"]
   }
 
+  egress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${var.accessip}"]
+  }
+
   tags {
     Name = "Openshift SSH"
   }
